ffb678f2c5cd06228bd75bcb5c7481650c984f1f
Ardak Boranov <Ardak.Boranov@intermobility.ru>
2023-09-07 13:53:22 +0600

    Init

---
+++ Otus/Otus/OtusApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtusApp.swift
+//  Otus
+//
+//  Created by Ardak Boranov on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct OtusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
